05cb86a2a56facc5bd01876b65b33f01ef7d696a
pepetorero2014 <98969049+pepetorero2014@users.noreply.github.com>
2022-02-08 12:12:58 -0800

    hola mundo inicial

---
+++ HolaMundo github/src/HolaMundo.java
@@ -0,0 +1,13 @@
+
+/* Este es un programa simple en Java...
+   NombreArchivo: "HelloWorld.java". */
+class HolaMundo
+{
+    // Tu programa comienza con una llamada a main().
+    // Imprime "Hola Mundo" a la ventana de la terminal.
+    public static void main(String args[])
+    {
+        System.out.println("Hola Mundo");
+        //añade aqui otro System.out.println con tu nombre completo
+    }
+}
